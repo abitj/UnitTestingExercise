f7d690e6d10c7cb6207859d9ce344af6267c1f99
abitj <https://github.com/abitj>
2021-02-15 16:12:41 -0600

    Wrote unit tests

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,96 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(2,3,5,10)]
+        [InlineData(12,10,3,25)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            var actual = test.Add(num1, num2, num3);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
-        public void Subtract(int minuend, int subtrhend, int expected)
+        [InlineData(7, 5, 2)]
+        [InlineData(100, 4, 96)]
+        public void Substract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            int actual = test.Substract(minuend, subtrhend);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
+
+
         [Theory]
-        [InlineData()]
+        [InlineData(7, 5, 35)]
+        [InlineData(100, 4, 400)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            int actual = test.Multiply(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(15, 5, 3)]
+        [InlineData(62, 2, 31)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            int actual = test.Divide(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
         public void YourMethodName()
         {
+
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            string actual = test.YourMethodName();
 
             //Assert
-
+            Assert.Equal("MethodNotFound", actual);
         }
 
         [Fact]
         public void YourMethodName2()
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
+            string actual = test.YourMethodName2();
 
             //Assert
+            Assert.Equal("HereMyMethod", actual);
         }
+
+
+
     }
 }
